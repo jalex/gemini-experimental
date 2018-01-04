67912b5d357acf7177571e5d5e3f219b3eafb38a
Jalex <jalex2006@gmail.com>
2018-01-04 15:49:33 +0300

    remove Xna and MonoGames projects

--- src/Gemini.Demo.MonoGame/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gemini.Demo.MonoGame
-{
-    /// <summary>
-    ///     Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
-    {
-    }
-}
--- src/Gemini.Demo.Xna/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gemini.Demo.Xna
-{
-    /// <summary>
-    ///     Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
-    {
-    }
-}
